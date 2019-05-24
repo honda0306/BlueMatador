422e8183442f9764bfda7c96771df064bdb656db
Chris Honda <christopher.honda92@gmail.com>
2019-05-24 13:08:03 -0700

    Initial pass at NorrisAPI

--- NorrisAPI.js
+++ NorrisAPI.js
@@ -4,13 +4,25 @@
 // The first function takes as input the joke category (string) and returns a joke (string) from the
 // API. If the category isn’t valid, according to the API, return null.
 
-function jokeCategory() {
-
-}
-
-// The second function takes as input a string, searches for jokes containing that string, and
-// returns one of the returned jokes.
-
-function jokeSearch() {
+function jokeCategory(str) {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${str}`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(contents) {
+            console.log(JSON.stringify(contents))
+        })
+    }
     
+    // The second function takes as input a string, searches for jokes containing that string, and
+    // returns one of the returned jokes.
+    
+    function jokeSearch(str) {
+        fetch(`https://api.chucknorris.io/jokes/search?query=${str}`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(contents) {
+            console.log(JSON.stringify(contents))
+        })
 }
